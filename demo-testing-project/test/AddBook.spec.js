47c903de05d384c5acf2b2a835893094c67d1d4b
Bogdan Dobrin <b.dobrin@yahoo.com>
2021-06-24 22:12:48 +0300

    Added tests for AddBook.vue and Book.vue

--- demo-testing-project/test/AddBook.spec.js
+++ demo-testing-project/test/AddBook.spec.js
@@ -14,6 +14,46 @@ describe('AddBook.vue', () => {
         expect(wrapper.vm.name).toEqual(name);
     });
 
+    it('Update data when inserting text in author input', () => {
+        const author = faker.name.findName();
+        const wrapper = shallowMount(AddBook);
+        const input = wrapper.find('.author');
+
+        input.setValue(author);
+
+        expect(wrapper.vm.author).toEqual(author);
+    });
+
+    it('Update data when inserting text in year input', () => {
+        const year = faker.datatype.number();
+        const wrapper = shallowMount(AddBook);
+        const input = wrapper.find('.year');
+
+        input.setValue(year);
+
+        expect(parseInt(wrapper.vm.year)).toEqual(year);
+    });
+
+    it('Update data when inserting text in category input', () => {
+        const category = faker.company.companyName();
+        const wrapper = shallowMount(AddBook);
+        const input = wrapper.find('.category');
+
+        input.setValue(category);
+
+        expect(wrapper.vm.category).toEqual(category);
+    });
+
+    it('Update data when inserting text in language input', () => {
+        const language = faker.name.findName();
+        const wrapper = shallowMount(AddBook);
+        const input = wrapper.find('.language');
+
+        input.setValue(language);
+
+        expect(wrapper.vm.language).toEqual(language);
+    });
+
     it('Event is emit when clicking Add button', () => {
         const handler = sinon.stub();
         const spy = sinon.spy(handler);
--- demo-testing-project/test/Book.spec.js
+++ demo-testing-project/test/Book.spec.js
@@ -1,14 +1,34 @@
 import {shallowMount} from '@vue/test-utils';
 import faker from 'faker';
+import sinon from 'sinon';
 import Book from '@/components/Book.vue';
 
 describe('Book.vue', () => {
+    it('should throw error if id is not provided', () => {
+        expect(() => shallowMount(Book, {
+            propsData: {
+                name: 'Amintiri din copilarie',
+                author: 'Ion Creanga'
+            }
+        }).toThrow(`[Vue warn]: Missing required prop: "id"`));
+    });
+
     it('should throw error if name is not provided', () => {
         expect(() => shallowMount(Book, {
             propsData: {
+                id: '1',
                 author: 'Ion Creanga'
             }
         }).toThrow(`[Vue warn]: Missing required prop: "name"`));
+    });
+
+    it('should throw error if author is not provided', () => {
+        expect(() => shallowMount(Book, {
+            propsData: {
+                id: '1',
+                name: 'Amintiri din copilarie'
+            }
+        }).toThrow(`[Vue warn]: Missing required prop: "author"`));
     });
 
     it('Book component should display the value of name prop', () => {
@@ -38,6 +58,22 @@ describe('Book.vue', () => {
                 language: faker.lorem.word()
             }
         }).toThrow());
+    });
+
+    it('emit remove event when clicking remove button', () => {
+        const handler = sinon.stub();
+        const spy = sinon.spy(handler);
+
+        const wrapper = shallowMount(Book, {
+            listeners: {
+                'remove': spy
+            }
+        });
+        const button = wrapper.find('.remove');
+
+        button.trigger('click');
+        
+        expect(spy.called).toEqual(true);
     });
 })
 
